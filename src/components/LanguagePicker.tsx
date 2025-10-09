704f7a3c400d9bcc38ce030511fe8876ae9d287d
Darko <darko.atanasov@gmail.com>
2025-10-09 21:59:53 +0200

    Updated LanguagePicker.tsx

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -14,7 +14,7 @@ export function LanguagePicker() {
   return (
     <button
       onClick={toggleLanguage}
-      className="px-2 py-2 rounded-full bg-transparent w-12 text-gray-400 hover:bg-[#444444] hover:text-white transition-colors"
+      className="px-2 py-2 rounded-full bg-transparent w-12 text-gray-400 hover:bg-[#333333] hover:text-white transition-colors"
     >
       {buttonText}
     </button>
