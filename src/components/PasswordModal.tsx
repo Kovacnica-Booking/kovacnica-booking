d506196958d3e599d79a78f09b62a68d52e168db
Darko <darko.atanasov@gmail.com>
2025-10-10 10:03:02 +0200

    Updated PasswordModal.tsx

--- src/components/PasswordModal.tsx
+++ src/components/PasswordModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+/*import React, { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { NumberInput } from '@/components/ui/number-input';
 
@@ -94,3 +94,4 @@ export function PasswordModal({ onAuthenticate }: PasswordModalProps) {
     </div>
   );
 }
+*/
