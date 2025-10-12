214b88a8acb72442cf4dcb76a09d5d410ec7fb22
Darko <darko.atanasov@gmail.com>
2025-10-12 18:05:46 +0200

    Updated PasswordModal.tsx

--- src/components/PasswordModal.tsx
+++ src/components/PasswordModal.tsx
@@ -34,6 +34,7 @@ export function PasswordModal({ onAuthenticate }: PasswordModalProps) {
         <LanguagePicker />
       </div>
       <div className="flex flex-col items-center px-4">
+          <Logo />
         <h2 className="text-2xl font-semibold mb-3">
           {t('auth.enterPassword')}
         </h2>
