f596ff0b416e09d655f6a77ccd883bd449ddd717
Darko <darko.atanasov@gmail.com>
2025-10-13 23:34:11 +0200

    Updated PasswordModal.tsx

--- src/components/PasswordModal.tsx
+++ src/components/PasswordModal.tsx
@@ -85,7 +85,7 @@ export function PasswordModal({ onAuthenticate }: PasswordModalProps) {
                 style={{ backgroundColor: 'transparent' }}
               />
               {showPassword && (
-                <Check className="absolute inset-0 w-4 h-4 text-white pointer-events-none" strokeWidth={3} />
+                <Check className="absolute inset-0 w-4 h-4 text-black pointer-events-none" strokeWidth={3} />
               )}
             </div>
             <span className="text-gray-400" style={{ fontSize: '14px' }}>
