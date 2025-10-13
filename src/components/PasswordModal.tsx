c0da2a787ce5cc3be91c9d6e5badcfaf8f8b8e7b
Darko <darko.atanasov@gmail.com>
2025-10-13 23:13:45 +0200

    Updated PasswordModal.tsx

--- src/components/PasswordModal.tsx
+++ src/components/PasswordModal.tsx
@@ -58,7 +58,7 @@ export function PasswordModal({ onAuthenticate }: PasswordModalProps) {
           } as React.CSSProperties}
         />
 
-        <form onSubmit={handleSubmit} className="flex mt-10 flex-col items-center w-xs">
+        <form onSubmit={handleSubmit} className="flex mt-10 flex-col items-center w-s">
           <input
             type="password"
             value={password}
